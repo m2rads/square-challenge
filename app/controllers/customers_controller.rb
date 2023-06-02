fa40b66bbacf8b00801bcb092c71c3b6c2a25b6a
m2rads <argo.mohrad@gmail.com>
2023-06-02 14:43:45 -0700

    change db for test env

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,21 @@
 class CustomersController < ApplicationController
-    def create
-      @customer = Customer.find_or_initialize_by(name: params[:name])
-      @store = Store.find_by(store_number: params[:store_number])
-  
-      if @customer.persisted? # Customer already exists
-        @customer.stores << @store unless @customer.stores.include?(@store)
-        @customer.update(image_url: @store.image_url)
-      else # Create a new customer
-        @customer.stores << @store
-        @customer.save
+  def create
+    @customer = Customer.find_or_initialize_by(name: params[:name])
+    @store = Store.find_by(store_number: params[:store_number])
+
+    if @store.nil?
+      render json: { error: "Store not found" }, status: :not_found
+    elsif @customer.persisted? # Customer already exists
+      @customer.stores << @store unless @customer.stores.include?(@store)
+      @customer.update(image_url: @store.image_url)
+      render json: @customer, status: :ok
+    else # Create a new customer
+      @customer.stores << @store
+      if @customer.save!
+        render json: @customer, status: :created
+      else
+        render json: { error: "Failed to create customer" }, status: :unprocessable_entity
       end
-  
     end
   end
-  
+end
