27da41888116854893d4db11642e554df10d696a
m2rads <argo.mohrad@gmail.com>
2023-06-02 10:50:47 -0700

    add cloudinary service as singlton

---
+++ app/services/cloudinary_service.rb
@@ -0,0 +1,26 @@
+require 'cloudinary'
+require 'cloudinary/uploader'
+require 'cloudinary/utils'
+require 'dot_env'
+
+class CloudinaryService
+  include Singleton
+
+  def initialize
+
+    Cloudinary.config do |config|
+      config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+      config.api_key = ENV['CLOUDINARY_API_KEY']
+      config.api_secret = ENV['CLOUDINARY_API_SECRET']
+      config.secure = true
+    end
+  end
+
+  def upload_image(url, public_id)
+    Cloudinary::Uploader.upload(url, public_id: public_id)
+  end
+
+  def transform_image(public_id, options = {})
+    Cloudinary::Utils.cloudinary_url(public_id, options)
+  end
+end
