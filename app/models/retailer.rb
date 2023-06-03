604dffcdcbf798b56a00f54a6276f05bb6d41353
m2rads <argo.mohrad@gmail.com>
2023-06-03 16:33:19 -0700

    add attributes to retailer model

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,4 +1,13 @@
 class Retailer < ApplicationRecord
     has_many :purchases
     has_many :customers, through: :purchases
+
+    attribute :business_name, :string
+    attribute :owner, :string
+    attribute :email, :string
+    attribute :phone, :string
+    attribute :image_url, :string
+    attribute :diameter, :float
+    attribute :coordinate_x, :float
+    attribute :coordinate_y, :float
 end
