cf8aa0bf4db0e0137790489a6e4e1976be6064a6
m2rads <argo.mohrad@gmail.com>
2023-06-03 16:33:02 -0700

    add attributes to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ApplicationRecord
     has_many :purchases
     has_many :retailers, through: :purchases
+
+    attribute :name, :string
+    attribute :last_name, :string
+    attribute :email, :string
+    attribute :phone, :string
+    attribute :age, :integer
 end
