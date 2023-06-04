8f80d1b6debce04cc4b86e2d68bc835006652053
m2rads <argo.mohrad@gmail.com>
2023-06-04 09:55:33 -0700

    update the unit test

--- test/controllers/square_controller_test.rb
+++ test/controllers/square_controller_test.rb
@@ -1,17 +1,18 @@
 require 'test_helper'
 
 class SquareControllerTest < ActionController::TestCase
-  test "should get authorization_url" do
+  test 'should get authorization_url' do
     get :authorization_url
     assert_response :success
-    assert_not_nil assigns(:authorization_url)
+    # Add additional assertions as needed
   end
 
-  test "should get callback" do
-    authorization_code = "12345" # Replace with a valid authorization code
-
-    post :callback, params: { code: authorization_code }
-    assert_response :success
-    assert_not_nil assigns(:access_token)
+  test 'should get callback' do
+    # Simulate the callback request with the authorization code
+    code = '12345'
+    get :callback, params: { code: code }
+    assert_redirected_to retailer_dashboard_path
+    assert_equal 'Square account linked successfully!', flash[:success]
+    # Add additional assertions as needed
   end
 end
